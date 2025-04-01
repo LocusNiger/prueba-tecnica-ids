7eedd6374c785638fb5eef5ecfa5ef0cfc6a6e2d
Iván Sánchez <sanchezivan.798@gmail.com>
2025-04-01 09:42:40 -0300

    Top sellers arrow

--- src/components/TopSellerCarrusel.jsx
+++ src/components/TopSellerCarrusel.jsx
@@ -1,3 +1,4 @@
+import { FaChevronDown, FaChevronRight, FaChevronLeft } from "react-icons/fa";
 import SellerCard from "./SellerCard";
 
 const TopSellerCarrusel = () => {
@@ -10,23 +11,30 @@ const TopSellerCarrusel = () => {
     { name: "Jenny Wilson", index: 6 },
   ];
   return (
-    <div className="w-full flex flex-col mt-10 py-2 gap-10 items-center justify-center border border-red-500">
-      <div className="border border-yellow-400 flex flex-col items-start w-[1100px] gap-6 max-w-7xl">
+    <div className="w-full flex mt-10 py-2 items-center justify-center">
+      <div className="border border-[#FFFFFF1A] bg-[#FFFFFF1A] p-2.5 rounded-full">
+        <FaChevronLeft className="text-[#FFFFFF]" />
+      </div>
+      <div className="border border-yellow-400 flex flex-col items-start w-[1110px] gap-6 max-w-7xl">
         {/* Textos */}
-        <div className="flex gap-2">
+        <div className="flex gap-2 items-center">
           <h3 className="text-3xl text-[#FFFFFF] font-manrope font-semibold">
             Top Seller In
           </h3>
           <h3 className="text-3xl text-[#DDF247] font-manrope font-semibold">
             1 day
           </h3>
+          <FaChevronDown className="h-3.5 w-3.5 text-[#e4ff40]" />
         </div>
         {/* Sellers */}
-        <div className="flex justify-between w-full">
+        <div className="flex justify-around w-full">
           {sellers.map((seller) => (
             <SellerCard seller={seller} key={seller.index} />
           ))}
         </div>
+      </div>
+      <div className="border border-[#DDF247] bg-[#DDF247] p-2.5 rounded-full">
+        <FaChevronRight className="text-[#161616]" />
       </div>
     </div>
   );
